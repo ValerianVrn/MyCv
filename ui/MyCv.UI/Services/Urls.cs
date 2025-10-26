56b9d0e6356c230107627529fc819f63bed5ae9d
Valérian VERONA <valerian.verona@fivesgroup.com>
2025-10-26 08:35:08 +0100

    feat: empty pages and links added

--- ui/MyCv.UI/Services/Urls.cs
+++ ui/MyCv.UI/Services/Urls.cs
@@ -4,10 +4,9 @@ namespace MyCv.UI.Services
     {
         public const string Home = "/";
         public const string Architecture = "/architecture";
+        public const string Coding = "/coding";
         public const string DevOps = "/devops";
         public const string Leadership = "/leadership";
-        public const string DotNetExpertise = "/dotnet";
-        public const string Skills = "/skills";
         public const string EducationAndCareer = "/career";
         public const string PersonalLife = "/life";
     }

222cdb2433aa57069492445d94cf0c4de06061e3
Valérian VERONA <valerian.verona@fivesgroup.com>
2025-10-28 08:21:57 +0100

    feat: language selection

---
+++ ui/MyCv.UI/Services/LocalizationService.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Components;
+using System.Globalization;
+
+namespace MyCv.UI.Services
+{
+    /// <inheritdoc/>
+    internal class LocalizationService(NavigationManager navigationManager) : ILocalizationService
+    {
+        public static readonly CultureInfo English = new("en-US");
+        public static readonly CultureInfo French = new("fr-FR");
+
+        /// <summary>
+        /// Navigation manager.
+        /// </summary>
+        private readonly NavigationManager _navigationManager = navigationManager;
+
+        /// <summary>
+        /// Current culture.
+        /// </summary>
+        private CultureInfo? _currentCulture;
+
+        /// <inheritdoc/>
+        public void SetCulture(CultureInfo cultureInfo)
+        {
+            _currentCulture = cultureInfo;
+        }
+
+        /// <inheritdoc/>
+        public void ApplySelectedCulture()
+        {
+            if (CultureInfo.CurrentCulture != _currentCulture)
+            {
+                var uri = new Uri(_navigationManager.Uri).GetComponents(UriComponents.PathAndQuery, UriFormat.Unescaped);
+                var cultureEscaped = Uri.EscapeDataString(_currentCulture?.Name ?? English.Name);
+                var uriEscaped = Uri.EscapeDataString(uri);
+
+                _navigationManager.NavigateTo($"Culture/Set?culture={cultureEscaped}&redirectUri={uriEscaped}", forceLoad: true);
+            }
+        }
+    }
+}

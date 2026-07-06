9192fe3c5cac183b71f9bd335d39996ca840a778
Valérian Verona <valerian.verona@gmail.com>
2026-07-06 15:42:49 +0200

    fix: DI missing

--- tailor/MyCv.Tailor.Api/Program.cs
+++ tailor/MyCv.Tailor.Api/Program.cs
@@ -6,8 +6,8 @@ using Microsoft.Extensions.Hosting;
 var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
-
 builder.Services
+    .AddHttpClient()
     .AddApplicationInsightsTelemetryWorkerService()
     .ConfigureFunctionsApplicationInsights();
 

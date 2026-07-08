c7f131a9119a099191e034d101803450d6d74186
Valérian Verona <valerian.verona@gmail.com>
2026-07-08 12:07:19 +0200

    feat: search bar tailor redesigned

--- ui/MyCv.UI.Wasm/Resources/Components/Pages/Home.Designer.cs
+++ ui/MyCv.UI.Wasm/Resources/Components/Pages/Home.Designer.cs
@@ -226,11 +226,38 @@ namespace MyCv.UI.Wasm.Resources.Components.Pages {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Try &quot;Azure tech lead for a fintech startup&quot; or &quot;Node.js team lead&quot;.
+        ///   Looks up a localized string similar to Analyse ✨.
+        /// </summary>
+        public static string TailorAnalyse {
+            get {
+                return ResourceManager.GetString("TailorAnalyse", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Analysing....
+        /// </summary>
+        public static string TailorAnalysing {
+            get {
+                return ResourceManager.GetString("TailorAnalysing", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Paste a full job offer or describe your ideal candidate.
         /// </summary>
         public static string TailorHint {
             get {
                 return ResourceManager.GetString("TailorHint", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Job offer.
+        /// </summary>
+        public static string TailorJobOffer {
+            get {
+                return ResourceManager.GetString("TailorJobOffer", resourceCulture);
             }
         }
         
@@ -244,11 +271,20 @@ namespace MyCv.UI.Wasm.Resources.Components.Pages {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Paste a job title or description — I&apos;ll tailor this CV for you.
+        ///   Looks up a localized string similar to Paste a job offer — I&apos;ll tailor this CV for you.
         /// </summary>
         public static string TailorPlaceholder {
             get {
                 return ResourceManager.GetString("TailorPlaceholder", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Tailor-made CV.
+        /// </summary>
+        public static string TailorTitle {
+            get {
+                return ResourceManager.GetString("TailorTitle", resourceCulture);
             }
         }
         
